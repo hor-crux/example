dc2d4603ad446d54c0c2ab4723c65d6a0c1cd5b8
oliverherrmann <o.herrmann92@googlemail.com>
2015-10-05 07:27:15 +0200

    change: us HcHref now

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ System.config({
     "github:hor-crux/horcrux-core@master": {
       "html": "github:hor-crux/html@master",
       "observejs": "github:polymer/observe-js@0.5.6",
-      "webcomponents": "github:webcomponents/webcomponentsjs@0.7.12"
+      "webcomponents": "github:webcomponents/webcomponentsjs@0.7.14"
     },
     "github:hor-crux/horcrux-di@master": {
       "horcrux-core": "github:hor-crux/horcrux-core@master"
